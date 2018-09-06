81303e560e708f73255f55f8d96098d1aea03129
Fernando Greco <greco-fernando@hotmail.com>
2018-09-05 21:25:26 -0300

    aula 61

--- src/app/alunos/alunos.module.ts
+++ src/app/alunos/alunos.module.ts
@@ -1,18 +1,21 @@
 import {NgModule} from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import {AlunosComponent} from './alunos.component';
 import {AlunoFormComponent} from './aluno-form/aluno-form.component';
 import { CommonModule } from '@angular/common';
 import { AlunosRoutingModule } from './alunos.routing.module';
 import {AlunoDetalheComponent} from './aluno-detalhe/aluno-detalhe.component';
+import { AlunosService } from './alunos.service';
 @NgModule ({
     imports: [
         CommonModule,
+        FormsModule,
         AlunosRoutingModule
     ],
     exports: [],
     declarations: [AlunosComponent, AlunoFormComponent,AlunoDetalheComponent],
-    providers: [],
+    providers: [AlunosService],
 })
 
 export class AlunosModule {
